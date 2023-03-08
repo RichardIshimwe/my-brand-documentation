000f95564b41beca725967e78db60418182a1028
rich26638 <ishimwerichard26@gmail.com>
2023-03-08 21:14:29 +0200

    resolving the server issues

--- src/documentation/server.js
+++ src/documentation/server.js
@@ -1,7 +1,8 @@
 const server = {
     servers: [
       {
-        url: "http://localhost:4000", // url
+        // url: "http://localhost:4000", // url
+        url: "https://puce-helpful-xerus.cyclic.app",
         description: "Local server", // name
       },
     ],
